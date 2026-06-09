61573729241fad70619bff5da38b2212aa3222e0
Edgar-Loop <mb7c5hz2zm@privaterelay.appleid.com>
2026-06-08 21:31:15 -0700

    Main menu added
    Have added a main menu to the game
    LopezEdgar_2DPolish_01

---
+++ Scenes/main_menu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+func _ready() -> void:
+	pass
+
+
+func _process(delta: float) -> void:
+	pass
+
+func _on_button_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/game.tscn")
+
+func _on_button_quit_pressed() -> void:
+	get_tree().quit()
